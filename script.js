c1a9f7bac5a42410b1e6dba191be41e90fd3ecf4
farzinahossainlopa <lopa152002@gmail.com>
2025-04-04 19:56:08 +0600

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Automatic age calculation
+function calculateAge(birthDate) {
+    const today = new Date();
+    const birthDateObj = new Date(birthDate);
+    let age = today.getFullYear() - birthDateObj.getFullYear();
+    const monthDiff = today.getMonth() - birthDateObj.getMonth();
+
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDateObj.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+document.getElementById("age").textContent = calculateAge("2002-05-15");
